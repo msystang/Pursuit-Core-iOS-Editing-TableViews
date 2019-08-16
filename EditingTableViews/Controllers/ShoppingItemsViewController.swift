46ed7c63566e8787223e27c64f2ee8e2bf0d0fd1
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-16 10:10:24 -0400

    adds tableview object into storyboard

--- EditingTableViews/Controllers/ShoppingItemsViewController.swift
+++ EditingTableViews/Controllers/ShoppingItemsViewController.swift
@@ -1,3 +1,20 @@
+/*
+- rename the view controller as "ShoppingItemsViewController" to be more descriptive
+- load all of the "ShoppingItems" into a table view in your ShoppingItemsViewController. the tableview should be pinned to the edges of its superview
+- set up data source and delegate for the tableview
+    - refactor some of the above so that they call the two private methods below in your viewDidLoad:
+
+private func loadShoppingItems() {
+    let allItems = ShoppingItemFetchingClient.getShoppingItems()
+    shoppingItems = allItems
+}
+
+private func configureShoppingItemsTableView() {
+    shoppingItemsTableView.dataSource = self
+    shoppingItemsTableView.delegate = self
+}
+*/
+
 import UIKit
 
 class ShoppingItemsViewController: UIViewController {
